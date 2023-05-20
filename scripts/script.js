c5c65e414c5211174294fc24ae380fbfa45e2d26
Ashokkumar <ashokkumarash96@gmail.com>
2023-05-20 12:34:28 +0530

    comments update

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,30 @@
 "use strict";
 
+// Get the main card element from the HTML document
 const mainCardEl = document.getElementById("mainCard");
 
+// Define an array of possible options for the game
 const option = ["rock", "paper", "scissor", "lizard", "spock"];
 
+// Initialize the scores for both players
 let youScore = 0;
 let computerScore = 0;
 
 // function
 
+// Generate a random option for the computer
 function computerOption() {
   return option[Math.floor(Math.random() * 5)];
 }
 
+// Update the score and display when the user wins a round
 function YouGetPoint() {
   document.getElementById("whoGettingScore").textContent = "You get 1 point";
   youScore++;
   document.getElementById("yourScore").textContent = youScore;
 }
 
+// Update the score and display when the computer wins a round
 function ComputerGetPoint() {
   document.getElementById("whoGettingScore").textContent =
     "Computer get 1 point";
@@ -26,43 +32,56 @@ function ComputerGetPoint() {
   document.getElementById("computerScore").textContent = computerScore;
 }
 
+// Compare the user's choice and the computer's choice and determine the winner
 function choice(userOption) {
   let UserChoice = userOption;
   let ComputerChoice = computerOption();
 
+  // If both choices are the same, it's a tie
   if (UserChoice === ComputerChoice) {
     document.getElementById("whoGettingScore").textContent = "No one get point";
   } else if (UserChoice === "rock") {
+    // Rock beats scissor and lizard
     if (ComputerChoice === "scissor" || ComputerChoice === "lizard") {
       YouGetPoint();
     } else {
+      // Rock loses to paper and spock
       ComputerGetPoint();
     }
   } else if (UserChoice === "paper") {
-    if (ComputerChoice === "scissor" || ComputerChoice === "lizard") {
-      ComputerGetPoint();
-    } else {
+    // Paper beats rock and spock
+    if (ComputerChoice === "rock" || ComputerChoice === "spock") {
       YouGetPoint();
+    } else {
+      // Paper loses to scissor and lizard
+      ComputerGetPoint();
     }
   } else if (UserChoice === "scissor") {
-    if (ComputerChoice === "rock" || ComputerChoice === "spock") {
-      ComputerGetPoint();
-    } else {
+    // Scissor beats paper and lizard
+    if (ComputerChoice === "paper" || ComputerChoice === "lizard") {
       YouGetPoint();
+    } else {
+      // Scissor loses to rock and spock
+      ComputerGetPoint();
     }
   } else if (UserChoice === "lizard") {
-    if (ComputerChoice === "rock" || ComputerChoice === "scissor") {
-      ComputerGetPoint();
-    } else {
+    // Lizard beats paper and spock
+    if (ComputerChoice === "paper" || ComputerChoice === "spock") {
       YouGetPoint();
+    } else {
+      // Lizard loses to rock and scissor
+      ComputerGetPoint();
     }
   } else if (UserChoice === "spock") {
-    if (ComputerChoice === "paper" || ComputerChoice === "lizard") {
-      ComputerGetPoint();
-    } else {
+    // Spock beats rock and scissor
+    if (ComputerChoice === "rock" || ComputerChoice === "scissor") {
       YouGetPoint();
+    } else {
+      // Spock loses to paper and lizard
+      ComputerGetPoint();
     }
   }
+  // Check if either player has reached 10 points and end the game
   if (youScore >= 10) {
     document.getElementById("wonPerson").textContent = "You";
     document.getElementById("option").classList.replace("option", "none");
@@ -80,6 +99,7 @@ function choice(userOption) {
   }
 }
 
+// Hide the main card and show a thank you message when the user clicks on no more game button
 function NoMoreGameFunction() {
   document.getElementById("mainCard").classList.add("none");
   document.getElementById("ThankYouMessage").classList.remove("none");
